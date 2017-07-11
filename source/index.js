3e5800ab90834e481be96e02877eb7ab5ddb92bb
Nathan Manceaux-Panot <cykelero@gmail.com>
2017-07-11 20:36:20 +0200

    Fix built-in definitions

--- source/index.js
+++ source/index.js
@@ -4,18 +4,18 @@ const rootSerializer = new Serializer('.');
 
 rootSerializer.register(Object, {
 	pack: v => Object.keys(v).reduce((result, key) => {
-		result[key] = Serializer.toPacked(v[key]);
+		result[key] = rootSerializer.toPacked(v[key]);
 		return result;
 	}, {}),
 	unpack: p => Object.keys(p).reduce((result, key) => {
-		result[key] = Serializer.fromPacked(p[key]);
+		result[key] = rootSerializer.fromPacked(p[key]);
 		return result;
 	}, {})
 });
 
 rootSerializer.register(Array, {
-	pack: v => v.map(i => Serializer.toPacked(i)),
-	unpack: p => p.map(i => Serializer.fromPacked(i))
+	pack: v => v.map(i => rootSerializer.toPacked(i)),
+	unpack: p => p.map(i => rootSerializer.fromPacked(i))
 });
 
 export { Serializer as Serializer };
